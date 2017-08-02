1c23d34acce81f9a9bb186722936a1315944dd66
kmder <kmder.coin@gmail.com>
2017-08-02 23:06:11 +0200

    Adds project first issue

---
+++ src/bg/background.js
@@ -0,0 +1,85 @@
+var stellarNetUrl = "https://horizon-testnet.stellar.org";
+var stellarServer = new StellarSdk.Server(stellarNetUrl);
+
+var defaultSettings = {
+        baseCurrency: "USD",
+        refreshRateInMinutes: "5",
+        account: ""
+    };
+
+var settings = {};
+
+var balance = 1;
+var price = 0;
+
+function formatBalance(num) {
+  if (num < 1000) {
+    var r = num.toString().split(".")[0].length;
+    return numeral(num).format(r == 1 ? "0.0000" : r == 2 ? "0.000" : "0.00");
+  }
+  else {
+    var r = (Math.floor(Math.log(num) / Math.LN10) + 1) % 3;
+    return numeral(num).format(r == 0 ? "0.0a" : r == 1 ? "0.000a" : "0.0a");
+  }
+  
+};
+
+function updateBadgeText(value) {
+    chrome.browserAction.setBadgeText({
+            text: formatBalance(value)
+        });
+}
+
+function updateBalance() {
+    var publicKey = settings.account;
+
+    if (publicKey) {
+        stellarServer.accounts()
+        .accountId(publicKey)
+        .call()
+        .then(function (accountResult) {
+            balance = parseFloat(accountResult.balances[0].balance);
+            updatePrice();
+        })
+        .catch(function (err) {
+            balance = 1;
+            updatePrice();
+        })
+    }
+    else {
+        balance = 1;
+        updatePrice();
+    }
+}
+
+function updatePrice() {
+    var baseCurrency = settings.baseCurrency;
+    var url = `https://api.coinmarketcap.com/v1/ticker/stellar/?convert=${baseCurrency}`;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+            var result = JSON.parse(xmlHttp.responseText);
+            price = parseFloat(result[0][`price_${baseCurrency.toLowerCase()}`]);
+            updateBadgeText(balance * price);
+        }
+    }
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send(null);
+}
+
+var settedTimeout;
+
+function run() {
+    chrome.storage.sync.get(defaultSettings, function (obj) {
+        if (settedTimeout) clearTimeout(settedTimeout)
+        settings = obj;
+        updateBalance();
+        settedTimeout = setTimeout(run, settings.refreshRateInMinutes * 1000 * 60);
+    });
+};
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    run();
+});
+
+run();
---
+++ src/options/js/options.js
@@ -0,0 +1,52 @@
+// Saves options to chrome.storage.sync.
+function save_options() {
+    var baseCurrency = document.getElementById('baseCurrency').value;
+    var refreshRateInMinutes = document.getElementById('refreshRate').value;
+    var account = document.getElementById('account').value;
+    chrome.storage.sync.set({
+        baseCurrency: baseCurrency,
+        refreshRateInMinutes: refreshRateInMinutes,
+        account : account
+    }, function () {
+    });
+}
+
+// Restores state using the preferences stored in chrome.storage.
+function restore_options() {
+    chrome.storage.sync.get({
+        baseCurrency: "USD",
+        refreshRateInMinutes: "5",
+        account: ""
+    }, function (items) {
+        document.getElementById('baseCurrency').value = items.baseCurrency;
+        document.getElementById('refreshRate').value = items.refreshRateInMinutes;
+        document.getElementById('account').value = items.account;
+    });
+}
+
+function registerOnChangeEventListner(elements) {
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('change', save_options);
+    }
+}
+
+function registerOnInputEventListner(elements) {
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('input', save_options);
+    }
+}
+function initView() {
+    var currencySelect = document.getElementById('baseCurrency');
+    for(var i = 0, l = currencies.length; i < l; i++) {
+        var option = currencies[i];
+        currencySelect.options.add( new Option(option, option, false) );
+    }
+
+    restore_options();
+}
+
+var currencies = ["USD", "EUR", "CHF", "GBP", "AUD", "BGN", "BRL", "CAD", "CNY", "CZK", "DKK", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "RUB", "SEK", "SGD", "THB", "TRY", "ZAR"];
+
+document.addEventListener('DOMContentLoaded', initView);
+registerOnChangeEventListner(document.getElementsByTagName('select'));
+document.getElementById('account').addEventListener('input', save_options);
